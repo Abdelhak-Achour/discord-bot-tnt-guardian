8c1c1a605d6b6db00fd635fce08d1e3235a5ed93
Abdelhak Achour <100156261+Abdelhak-Achour@users.noreply.github.com>
2022-02-21 23:20:59 +0100

    Add files via upload

---
+++ nimo.js
@@ -0,0 +1,9 @@
+module.exports =
+{
+    name: 'nimo',
+    description: 'give the Nimo channel link of Mr Devil TNT',
+    execute(message)
+    {
+        message.channel.send("Nimo coming soon");
+    }
+}
