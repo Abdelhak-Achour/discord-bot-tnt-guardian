b60bcc0ca9471d0099d3f71241bad83048849612
Abdelhak Achour <100156261+Abdelhak-Achour@users.noreply.github.com>
2022-02-22 22:50:57 +0100

    Delete nimo.js

--- nimo.js
+++
@@ -1,9 +0,0 @@
-module.exports =
-{
-    name: 'nimo',
-    description: 'give the Nimo channel link of Mr Devil TNT',
-    execute(message)
-    {
-        message.channel.send("Nimo coming soon");
-    }
-}
