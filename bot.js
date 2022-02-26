6b46b366a6841ecf56420ab75b971443727b7894
Abdelhak Achour <achourabdelhak2001@gmail.com>
2022-02-26 20:35:30 +0100

    set up welcome event handler to send embed if user is human and basic role and for bots add bots role only and no msg

--- bot.js
+++ bot.js
@@ -32,9 +32,23 @@ client.once('ready', () =>
 
 client.on('guildMemberAdd', guildMember =>
 {
-    const memberRoleID = guildMember.guild.roles.cache.find(role => role.name === 'TNT lovers').id;
-    guildMember.roles.add(memberRoleID);
-    guildMember.guild.channels.cache.get('801455167056510976').send(`<@${guildMember.user.id}> joined and given role.`);
+    if (guildMember.user.bot)
+    {
+        const botRoleID = guildMember.guild.roles.cache.find(role => role.name === 'Bots').id;
+        guildMember.roles.add(botRoleID);
+    }
+    else
+    {
+        const memberRoleID = guildMember.guild.roles.cache.find(role => role.name === 'TNT lovers').id;
+        guildMember.roles.add(memberRoleID);
+        const welcomeEmbed = new Discord.MessageEmbed()
+        .setColor('#D82148')
+        .setTitle('A new TNT lover joined the server!')
+        .setDescription(`${guildMember}, welcome to our server.\n**o(◑ω◐)づ**\nmake sure to read our <#943600351561531542>,\nread <#943602904965722212> for info about our channels\nand please enjoy your stay.`)
+        .setImage('https://images-ext-1.discordapp.net/external/Z66q3rXhdUNIFo-3gDOyHUJE0GU0vSQuZt6plRfnKNA/https/cdn-longterm.mee6.xyz/plugins/welcome/images/943595019770691671/fd101b6326c5b8487d49b2c398534f1067783f3287b0371c30070107ea776f95.jpeg')
+        .setThumbnail(guildMember.user.displayAvatarURL());
+        guildMember.guild.channels.cache.get('943628384938897429').send({embeds: [welcomeEmbed]});
+    }
 });
 
 client.on("messageCreate", message =>
