65affc8f9211739e08a3f338716ec35378a11a9e
Abdelhak Achour <achourabdelhak2001@gmail.com>
2022-03-01 19:35:31 +0100

    removed console logs nd added blocs of try catch so the bot keeps on working even if an error occurs

--- bot.js
+++ bot.js
@@ -55,160 +55,164 @@ client.on('guildMemberAdd', guildMember =>
 
 client.on('guildMemberRemove', guildMember =>
 {
-    if (guildMember.user.bot) return;
+    try
+    {
+        if (guildMember.user.bot) return;
     guildMember.guild.channels.cache.get('943934793907843102').send(`૮₍ ˃ ⤙ ˂ ₎ა\n./づ︻┻┳══━一             **${guildMember.user.tag}** left the server.`);
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
 });
 
 client.on("messageCreate", message =>
 {
-    let warn = false;
-    if(message.author.bot) return;
-    msg = message.content.toLowerCase();
-    if ((message.author.id === '474269529543934003' || message.author.id === '569224361265856552' || message.author.id === '816409290217750538') && msg === 'turn automod on/off')
+    try
     {
-        automod = !automod;
-        message.reply('automod turned on');
-        return;
-    }
-    if (msg === "never gonna")
-    {
-        message.channel.send("give you up");
-        return;
-    }
-    else if (msg === "i need tnt")
-    {
-        message.channel.send("here take some");
-        return;
-    }
-    else if (!msg.startsWith(prefix) && automod) // add code here to make it ignore some trusted users
-    {
-        // detection code
-        fs.readFileSync('nsfw.json', (error, data) =>
+        let warn = false;
+        if(message.author.bot) return;
+        msg = message.content.toLowerCase();
+        if ((message.author.id === '474269529543934003' || message.author.id === '569224361265856552' || message.author.id === '816409290217750538') && msg === 'turn automod on/off')
         {
-            let nsfw = JSON.parse(data);
-            
-        });
-        try
-        {
-            let nsfw = JSON.parse(fs.readFileSync('nsfw.json'));
-            for (let word of nsfw.notAllowedWords)
-            {
-                if (msg.includes(word))
-                {
-                    warn = true;
-                    console.log(warn);
-                    break;
-                }
-            }
+            automod = !automod;
+            message.reply('automod turned on');
+            return;
         }
-        catch
+        if (msg === "never gonna")
         {
-            console.log("error reading file.");
+            message.channel.send("give you up");
+            return;
         }
-        console.log(warn);
-        // action code
-        if (warn)
+        else if (msg === "i need tnt")
         {
-            console.log("first");
-            let memberRolesIDsList = [];
-            let timer = "0s";
-            fs.readFile('warns.json', (error, data) =>
+            message.channel.send("here take some");
+            return;
+        }
+        else if (!msg.startsWith(prefix) && automod && (message.author.id === '474269529543934003' || message.author.id === '816409290217750538' || message.author.id === '569224361265856552'))
+        {
+            // detection code
+            fs.readFileSync('nsfw.json', (error, data) =>
             {
-                let warns = JSON.parse(data);
-                console.log("reading warns");
-                if (message.author.id in warns.id)
-                {
-                    console.log("already existing id");
-                    warns.id[message.author.id]++;
-                    if (warns.id[message.author.id] >= 2 && warns.id[message.author.id] < 7)
-                    {
-                        console.log("muting");
-                        message.delete().then(message.channel.send(`${message.member} warned and muted.`)).catch();
-                        const mutedID = message.guild.roles.cache.find(role => role.name === 'muted').id;
-                        for (let x = 0;x<message.member.roles.cache.size-1;x++)
-                        {
-                            memberRolesIDsList.push(message.member.roles.cache.at(x).id);
-                        }
-                        for (let y = 0;y<message.member.roles.cache.size-1;y++)
-                        {
-                            message.member.roles.remove(message.member.roles.cache.at(y).id);
-                        }
-                        message.member.roles.add(mutedID);
-                        fs.writeFile("warns.json", JSON.stringify(warns), function(error, result){if (error){console.log(error);}});
-                        const timers = ["5m", "30m", "3h", "24h", "7d"];
-                        timer = timers[warns.id[message.author.id]-2];
-                        console.log("timer set");
-                    }
-                    else if (warns.id[message.author.id] >= 7)
-                    {
-                        console.log("banning");
-                        message.delete().then(message.guild.members.ban(message.author.id)).catch();
-                        warns.id[message.author.id] = 0;
-                        fs.writeFile("warns.json", JSON.stringify(warns), function(error, result){if (error){console.log(error);}});
-                    }
-                }
-                else
-                {
-                    console.log("not already existing id");
-                    warns.id[message.author.id] = 1;
-                    message.delete().then(message.channel.send(`${message.member} warned.`)).catch();
-                    fs.writeFile("warns.json", JSON.stringify(warns), function(error, result){if (error){console.log(error);}});
-                } // test it on other users cuz it can't mute u
+                let nsfw = JSON.parse(data);
+                
             });
-            if (!(timer === "0s"))
+            try
             {
-                console.log("starting timeout");
-                setTimeout(function()
+                let nsfw = JSON.parse(fs.readFileSync('nsfw.json'));
+                for (let word of nsfw.notAllowedWords)
                 {
-                    message.member.roles.remove(mutedID);
-                    for (let z = 0;z<memberRolesIDsList.length;z++)
+                    if (msg.includes(word))
                     {
-                        message.member.roles.add(memberRolesIDsList[z]);
+                        warn = true;
+                        break;
                     }
-                }, ms(timer)
-                );
+                }
             }
+            catch
+            {
+                console.log("error reading file.");
+            }
+            // action code
+            if (warn)
+            {
+                let memberRolesIDsList = [];
+                let timer = "0s";
+                fs.readFile('warns.json', (error, data) =>
+                {
+                    let warns = JSON.parse(data);
+                    if (message.author.id in warns.id)
+                    {
+                        warns.id[message.author.id]++;
+                        if (warns.id[message.author.id] >= 2 && warns.id[message.author.id] < 7)
+                        {
+                            message.delete().then(message.channel.send(`${message.member} warned and muted.`)).catch();
+                            const mutedID = message.guild.roles.cache.find(role => role.name === 'muted').id;
+                            for (let x = 0;x<message.member.roles.cache.size-1;x++)
+                            {
+                                memberRolesIDsList.push(message.member.roles.cache.at(x).id);
+                            }
+                            for (let y = 0;y<message.member.roles.cache.size-1;y++)
+                            {
+                                message.member.roles.remove(message.member.roles.cache.at(y).id);
+                            }
+                            message.member.roles.add(mutedID);
+                            fs.writeFile("warns.json", JSON.stringify(warns), function(error, result){if (error){console.log(error);}});
+                            const timers = ["5m", "30m", "3h", "24h", "7d"];
+                            timer = timers[warns.id[message.author.id]-2];
+                        }
+                        else if (warns.id[message.author.id] >= 7)
+                        {
+                            message.delete().then(message.guild.members.ban(message.author.id)).catch();
+                            warns.id[message.author.id] = 0;
+                            fs.writeFile("warns.json", JSON.stringify(warns), function(error, result){if (error){console.log(error);}});
+                        }
+                    }
+                    else
+                    {
+                        warns.id[message.author.id] = 1;
+                        message.delete().then(message.channel.send(`${message.member} warned.`)).catch();
+                        fs.writeFile("warns.json", JSON.stringify(warns), function(error, result){if (error){console.log(error);}});
+                    } // test it on other users cuz it can't mute u
+                });
+                if (!(timer === "0s"))
+                {
+                    setTimeout(function()
+                    {
+                        message.member.roles.remove(mutedID);
+                        for (let z = 0;z<memberRolesIDsList.length;z++)
+                        {
+                            message.member.roles.add(memberRolesIDsList[z]);
+                        }
+                    }, ms(timer)
+                    );
+                }
+            }
+            return;
         }
-        return;
+        if(!msg.startsWith(prefix)) return;
+        const [command, ...args] = msg.trim().substring(prefix.length).split(/\s+/);
+        if (!client.commands.has(command)) return message.reply("I have no such command.");
+        if(command === 'ping')
+        {
+            client.commands.get(command).execute(message, args);
+        }
+        else if (command === 'youtube')
+        {
+            client.commands.get(command).execute(message);
+        }
+        else if (command === 'nimo')
+        {
+            client.commands.get(command).execute(message);
+        }
+        else if (command === 'echo')
+        {
+            client.commands.get(command).execute(message, args);
+        }
+        else if (command === 'kick')
+        {
+            client.commands.get(command).execute(message, args);
+        }
+        else if (command === 'ban')
+        {
+            client.commands.get(command).execute(message, args);
+        }
+        else if (command === 'rules')
+        {
+            client.commands.get(command).execute(message, Discord);
+        }
+        else if (command === 'clear')
+        {
+            client.commands.get(command).execute(message, args);
+        }
+        else if (command === 'mute')
+        {
+            client.commands.get(command).execute(message, args);
+        }
     }
-    if(!msg.startsWith(prefix)) return;
-    const [command, ...args] = msg.trim().substring(prefix.length).split(/\s+/);
-    if (!client.commands.has(command)) return message.reply("I have no such command.");
-    if(command === 'ping')
+    catch (error)
     {
-        client.commands.get(command).execute(message, args);
-    }
-    else if (command === 'youtube')
-    {
-        client.commands.get(command).execute(message);
-    }
-    else if (command === 'nimo')
-    {
-        client.commands.get(command).execute(message);
-    }
-    else if (command === 'echo')
-    {
-        client.commands.get(command).execute(message, args);
-    }
-    else if (command === 'kick')
-    {
-        client.commands.get(command).execute(message, args);
-    }
-    else if (command === 'ban')
-    {
-        client.commands.get(command).execute(message, args);
-    }
-    else if (command === 'rules')
-    {
-        client.commands.get(command).execute(message, Discord);
-    }
-    else if (command === 'clear')
-    {
-        client.commands.get(command).execute(message, args);
-    }
-    else if (command === 'mute')
-    {
-        client.commands.get(command).execute(message, args);
+        console.log(error);
     }
 }
 );
